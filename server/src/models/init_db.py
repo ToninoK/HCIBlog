2cefc629c6c960ee3e5470b22642090059c51aa0
Tonino Kaštelan <37194395+ToninoK@users.noreply.github.com>
2023-01-04 20:24:38 +0100

    initialize db models

---
+++ server/src/models/init_db.py
@@ -0,0 +1,49 @@
+from src.models.connection import conn
+
+CREATE_TABLES_QUERY = """
+    CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        first_name VARCHAR(80),
+        last_name VARCHAR(80),
+        username VARCHAR(80),
+        password VARCHAR(120),
+        email VARCHAR(80),
+        description VARCHAR(512),
+        facebook VARCHAR(512),
+        github VARCHAR(512),
+        linkedin VARCHAR(512),
+        instagram VARCHAR(512),
+        created_at DATE,
+        updated_at DATE
+    );
+
+    CREATE TABLE IF NOT EXISTS posts (
+        id SERIAL PRIMARY KEY,
+        user_id INT REFERENCES users(id),
+        title VARCHAR(80),
+        content JSONB,
+        tags TEXT[],
+        created_at DATE,
+        updated_at DATE
+    );
+
+    CREATE TABLE IF NOT EXISTS tags (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(80) UNIQUE,
+        post_id INT REFERENCES posts(id),
+        created_at DATE,
+        updated_at DATE
+    );
+
+    CREATE INDEX IF NOT EXISTS tag_name_idx ON tags (name);
+"""
+
+
+async def create_tables():
+    async with (conn.cursor()) as cursor:
+        try:
+            await cursor.execute(CREATE_TABLES_QUERY)
+        except Exception as e:
+            print(e)
+            raise Exception from e
+        print("Tables successfully created")
