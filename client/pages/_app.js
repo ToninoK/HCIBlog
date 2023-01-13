01b8885b17e2bf0d2e18387608d9591c7c81b74b
tkastelan <tonino.kastelan1@gmail.com>
2023-01-13 23:47:27 +0100

    test if cors works

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  fetch('https://api.hciblog.online/ping')
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   return <Component {...pageProps} />
 }
 
