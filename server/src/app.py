c943ed7edd7c05a8d92bd2e91802529de3cb1a40
Tonino Kaštelan <37194395+ToninoK@users.noreply.github.com>
2023-01-14 00:01:36 +0100

    BE deployment

--- server/src/app.py
+++ server/src/app.py
@@ -3,8 +3,11 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from src.models.init_db import create_tables
 from src.routers import users, posts, tags
+from src.settings import config
 
-app = FastAPI(root_path="/api")
+ROOT_PATH = "/api" if config.DEBUG else "/"
+
+app = FastAPI(root_path=ROOT_PATH)
 
 routers = [
     posts,
@@ -15,11 +18,11 @@ routers = [
 for router in routers:
     app.include_router(router)
 
-origins = []
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["localhost"],
+    allow_origin_regex='https://hci-blog(-.*-)?((matejkozica)|(toninok))?\.vercel\.app',
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- server/src/settings.py
+++ server/src/settings.py
@@ -4,12 +4,12 @@ from os import environ
 class Config:
     # Server
     SECRET_KEY = environ.get("SECRET_KEY")
-    DEBUG = bool(environ.get("DEBUG", True))
+    DEBUG = bool(environ.get("DEBUG", False))
 
     # Database
     POSTGRES_HOST = environ.get("POSTGRES_HOST")
-    POSTGRES_DB = environ.get("POSTGRES_DB", "student_projects")
-    POSTGRES_USER = environ.get("POSTGRES_USER", "admin")
+    POSTGRES_DB = environ.get("POSTGRES_DB", "blog_db")
+    POSTGRES_USER = environ.get("POSTGRES_USER", "blogger")
     POSTGRES_PASSWORD = environ.get("POSTGRES_PASSWORD", "")
 
     # Redis
