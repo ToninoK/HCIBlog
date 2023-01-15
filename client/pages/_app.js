529bbf4ab29b327aa199c4589cf8bb48fe0959b9
Matej Kozica <matejkozica@Matejs-MacBook-Pro-4.local>
2023-01-15 16:42:57 +0100

    Add api

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,10 +1,7 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  fetch('https://api.hciblog.online/ping')
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
