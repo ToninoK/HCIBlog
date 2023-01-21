2e50806cc453a20cbb51785cdd8a103998899a84
MatejKozica <matej.split@gmail.com>
2023-01-21 18:21:44 +0100

    Change tags internal structure

--- client/components/Tags/index.js
+++ client/components/Tags/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Card, Badge, Flex, UnstyledButton, Text } from "@mantine/core";
 
-const Tags = ({ tags, onClick, selectedTags }) => {
+import usePosts from "../../services/posts/usePosts";
+
+const Tags = () => {
+  const { tags, selectedTags, toggleSelectedTags } = usePosts();
+
   return (
     <Card shadow="sm" p="lg" mb="xl" radius="md" withBorder>
-      <Text weight="bold" size="lg">
+      <Text weight="bold" size="lg" mb="lg">
         Popular tags
       </Text>
       <Flex
@@ -16,11 +20,14 @@ const Tags = ({ tags, onClick, selectedTags }) => {
       >
         {tags?.map((tag) => {
           return (
-            <UnstyledButton key={tag.name} onClick={() => onClick(tag.name)}>
+            <UnstyledButton
+              key={tag.name}
+              onClick={() => toggleSelectedTags(tag.name)}
+            >
               <Badge
                 size="lg"
                 color="cyan"
-                variant={selectedTags.includes(tag.name) ? "filled" : "light"}
+                variant={selectedTags?.includes(tag.name) ? "filled" : "light"}
               >
                 #{tag.name}
               </Badge>
