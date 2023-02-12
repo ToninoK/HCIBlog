51bb510efb7c8ae1fe3c321465acf37b9c1df184
Matej Kožica <39216604+MatejKozica@users.noreply.github.com>
2023-02-12 16:06:27 +0100

    Add about page

--- client/pages/about.js
+++ client/pages/about.js
@@ -1,5 +1,81 @@
+import { useState, useEffect } from "react";
+import {
+  Card,
+  Text,
+  Flex,
+  Grid,
+  Title,
+  Avatar,
+  ActionIcon,
+  TypographyStylesProvider,
+} from "@mantine/core";
+import {
+  IconBrandFacebook,
+  IconBrandInstagram,
+  IconBrandLinkedin,
+  IconBrandGithub,
+} from "@tabler/icons";
+
+import useUser from "../services/users/useUser";
+
 const About = () => {
-  return <h1>About</h1>;
+  const { user, userLoading, getUser } = useUser();
+
+  useEffect(() => {
+    getUser("nickarmie@gmail.com");
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  console.log("nesto", user);
+
+  return (
+    <>
+      <Title order={1} weight="bold" mb="lg">
+        About
+      </Title>
+      <Grid justify="space-between">
+        <Grid.Col span={7}>
+          <Card shadow="sm" p="lg" radius="md" withBorder mb="lg">
+            <TypographyStylesProvider>
+              <div dangerouslySetInnerHTML={{ __html: user?.description }} />
+            </TypographyStylesProvider>
+          </Card>
+          <Flex direction="row" align="center" gap="lg">
+            <a target="_blank" href={user?.facebook} rel="noreferrer">
+              <ActionIcon variant="default">
+                <IconBrandFacebook size={28} />
+              </ActionIcon>
+            </a>
+            <a target="_blank" href={user?.instagram} rel="noreferrer">
+              <ActionIcon variant="default">
+                <IconBrandInstagram size={28} />
+              </ActionIcon>
+            </a>
+            <a target="_blank" href={user?.linkedin} rel="noreferrer">
+              <ActionIcon variant="default">
+                <IconBrandLinkedin size={28} />
+              </ActionIcon>
+            </a>
+            <a target="_blank" href={user?.github} rel="noreferrer">
+              <ActionIcon variant="default">
+                <IconBrandGithub size={28} />
+              </ActionIcon>
+            </a>
+          </Flex>
+        </Grid.Col>
+        <Grid.Col span={2}>
+          <Avatar src={user?.profile} mb="lg" size={128} />
+          <Text
+            size="lg"
+            weight="bold"
+          >{`${user?.first_name} ${user?.last_name}`}</Text>
+          <Text size="md" c="dimmed">
+            {user?.email}
+          </Text>
+        </Grid.Col>
+      </Grid>
+    </>
+  );
 };
 
 export default About;
--- client/services/auth/index.js
+++ client/services/auth/index.js
@@ -1,27 +1,27 @@
 import api from "../../pages/api";
-import Cookies from "js-cookie"
+import Cookies from "js-cookie";
 
 export const getAuthHeader = () => {
-    const currentUser = Cookies.get("currentUser")
-    if (currentUser) {
-        return {Authorization: "Bearer " + JSON.parse(currentUser).accessToken}
-    }
-    return {Authorization: ""}
-}
+  const currentUser = Cookies.get("currentUser");
+  if (currentUser) {
+    return { Authorization: "Bearer " + JSON.parse(currentUser).accessToken };
+  }
+  return { Authorization: "" };
+};
 
 export const login = async (data) => {
-    const resp = await api.post("/users/login", data)
-    return {
-        status: resp.status,
-        data: {
-            accessToken: resp.data.access_token,
-            username: resp.data.email,
-            expiredAt: resp.data.expires_at,
-        }
-    }
-}
+  const resp = await api.post("/users/login", data);
+  return {
+    status: resp.status,
+    data: {
+      accessToken: resp.data.access_token,
+      username: resp.data.email,
+      expiredAt: resp.data.expires_at,
+    },
+  };
+};
 
 export const logout = async () => {
-    const resp = await api.get("/users/logout")
-    return resp
-}
+  const resp = await api.get("/users/logout");
+  return resp;
+};
