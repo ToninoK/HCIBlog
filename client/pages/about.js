cea10de3e03e8a4410b714d6f385ac641316ccc9
MatejKozica <matej.split@gmail.com>
2023-02-14 20:20:46 +0100

    Fix about you

--- client/pages/about.js
+++ client/pages/about.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import {
   Card,
   Text,
@@ -28,11 +28,11 @@ const About = () => {
 
   return (
     <>
-      <Title order={1} weight="bold" mb="lg">
+      <Title weight="bold" mb="lg">
         About
       </Title>
       <Grid justify="space-between">
-        <Grid.Col span={6}>
+        <Grid.Col order={2} md={8} lg={6} sm={12}>
           <Card shadow="sm" p="lg" radius="md" withBorder mb="lg">
             <TypographyStylesProvider>
               <div dangerouslySetInnerHTML={{ __html: user?.description }} />
@@ -40,33 +40,43 @@ const About = () => {
           </Card>
           <Flex direction="row" align="center" gap="lg">
             <a target="_blank" href={user?.facebook} rel="noreferrer">
-              <ActionIcon variant="default">
-                <IconBrandFacebook size={32} />
+              <ActionIcon variant="default" size="xl">
+                <IconBrandFacebook size={34} />
               </ActionIcon>
             </a>
             <a target="_blank" href={user?.instagram} rel="noreferrer">
-              <ActionIcon variant="default">
-                <IconBrandInstagram size={32} />
+              <ActionIcon variant="default" size="xl">
+                <IconBrandInstagram size={34} />
               </ActionIcon>
             </a>
             <a target="_blank" href={user?.linkedin} rel="noreferrer">
-              <ActionIcon variant="default">
-                <IconBrandLinkedin size={32} />
+              <ActionIcon variant="default" size="xl">
+                <IconBrandLinkedin size={34} />
               </ActionIcon>
             </a>
             <a target="_blank" href={user?.github} rel="noreferrer">
-              <ActionIcon variant="default">
-                <IconBrandGithub size={32} />
+              <ActionIcon variant="default" size="xl">
+                <IconBrandGithub size={34} />
               </ActionIcon>
             </a>
           </Flex>
         </Grid.Col>
-        <Grid.Col span={3}>
+        <Grid.Col
+          order={1}
+          orderLg={2}
+          orderMd={2}
+          orderSm={1}
+          md={3}
+          lg={3}
+          sm={12}
+        >
           <Avatar
             src={user?.profile && `data:image/png;base64, ${user?.profile}`}
             mb="lg"
             size={128}
+            mr="lg"
           />
+
           <Text
             size="lg"
             weight="bold"
